86f84aceb15baa3cdee1ad2ed498def913cf0024
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-07-07 00:53:01 +0530

    routes protected

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,45 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
+import ProtectedRoute from "./components/ProtectedRout";
+import { isLoggedIn } from "./service/TokenService";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route
+          path="/"
+          element={
+            isLoggedIn() ? (
+              <Navigate to="/dashboard" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
 
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
         {/* Dashboards */}
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["USER", "ADMIN"]}>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
 
         <Route path="*" element={<Navigate to="/register" replace />} />
       </Routes>
